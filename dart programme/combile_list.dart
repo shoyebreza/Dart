aa39055630ea4c4d8de12fbd2c2f1c3d5a70ee0b
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2022-11-03 20:10:26 +0600

    combine list

---
+++ dart programme/combile_list.dart
@@ -0,0 +1,13 @@
+// Main function
+main() {
+	
+// Creating lists
+List gfg1 = ['Welcome','to'];
+List gfg2 = ['GeeksForGeeks'];
+	
+// Combining lists
+gfg1.addAll(gfg2);
+	
+// Printing combined list
+print(gfg1);
+}
---
+++ dart programme/operatorCombine.dart
@@ -0,0 +1,14 @@
+// Main function
+main() {
+	
+// Creating lists
+List gfg1 = ['Welcome'];
+List gfg2 = ['to'];
+List gfg3 = ['GeeksForGeeks'];
+	
+// Combining lists
+var newgfgList = gfg1 + gfg2 + gfg3;
+	
+// Printing combined list
+print(newgfgList);
+}
