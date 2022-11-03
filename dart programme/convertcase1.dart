50b6efdd9880a871e6829e7a91985a60477a2b0f
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2022-11-03 23:45:21 +0600

    case convert

---
+++ dart programme/convertcase1.dart
@@ -0,0 +1,14 @@
+void main() {
+	
+// str1 stores the string "geeks"
+String Str1 = "geeks";
+
+// str2 stores the string "fOr GEeks"
+String Str2 = "fOr GEeks";
+	
+// print str1 in uppercase letter
+print(Str1.toUpperCase());
+	
+// print str2 in uppercase letter
+print(Str2.toUpperCase());
+}
---
+++ dart programme/convertcase2.dart
@@ -0,0 +1,14 @@
+void main() {
+
+// str1 stores the string "COMPUTER"
+String Str1 = "COMPUTER";
+
+// str2 stores the string "laPToP"
+String Str2 = "laPToP";
+	
+// print str1 in uppercase letter
+print(Str1.toUpperCase());
+	
+// print str2 in uppercase letter
+print(Str2.toUpperCase());
+}
