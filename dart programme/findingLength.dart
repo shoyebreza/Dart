51d391853b6a5dcc91b544e28f9263739046e539
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2022-11-03 22:46:26 +0600

    finding length

---
+++ dart programme/findingLength.dart
@@ -0,0 +1,12 @@
+// Main function
+main() {
+	
+// Declaring a string variable
+String gfg = "Welcome to GeeksForGeeks";
+	
+// Finding the length
+int length = gfg.length;
+	
+// Printing the length of the string
+print(length);
+}
