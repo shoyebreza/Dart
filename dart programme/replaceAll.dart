a601ec95958652a82366cff21df48ca198b91897
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2022-11-04 20:45:30 +0600

    replace string

---
+++ dart programme/replaceAll.dart
@@ -0,0 +1,9 @@
+// Main function
+main() {
+	String gfg = "Welcome GeeksForGeeks";
+	
+	//replace substring of the given string
+	String result = gfg.replaceAll("GeeksForGeeks", "Geek!").replaceAll("!", " :)");
+	
+	print(result);
+}
