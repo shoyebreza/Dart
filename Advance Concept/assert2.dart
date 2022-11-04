362a6da42c9a7732cb0dcff24bcb81ee3df92fae
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2022-11-04 23:02:45 +0600

    asser satatement

---
+++ Advance Concept/assert2.dart
@@ -0,0 +1,6 @@
+void main()
+{
+String geek = "Geeks For Geeks";
+assert(geek != "Geeks For Geeks", "Strings are equal So this message is been displayed!!");
+print("You Can See This Line Geek as a Output if assert returns true");
+}
---
+++ Advance Concept/assertStatement.dart
@@ -0,0 +1,6 @@
+void main()
+{
+String geek = "Geeks For Geeks";
+assert(geek != "Geeks For Geeks");
+print("You Can See This Line Geek as a Output");
+}
