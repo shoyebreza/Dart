7392ea99fc7ec0c51da0349302f56d910f393d15
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2022-11-04 23:19:20 +0600

    dart practice

---
+++ Advance Concept/fallthroughCondition.dart
@@ -0,0 +1,17 @@
+void main() {
+int gfg = 1;
+switch ( gfg ){
+	case 1:{
+	print("GeeksforGeeks number 1");
+	} continue next;
+	next:case 2:{
+	print("GeeksforGeeks number 2");
+	} break;
+	case 3:{
+	print("GeeksforGeeks number 3");
+	} break;
+	default :{
+	print("This is default case");
+	} break;
+}
+}
